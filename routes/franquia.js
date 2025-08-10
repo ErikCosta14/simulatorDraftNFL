4e3751b2df29d54bce1b494000cc7d59ddd087cc
ErikCosta14 <dg09662@gmail.com>
2025-08-10 19:36:15 -0300

    Atualizacao rápida

--- routes/franquia.js
+++ routes/franquia.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
 var controllerFranquia = require('../Controller/controllerFranquia');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 /* GET */
 router.get('/verdetalhes/:idFranq', controllerFranquia.verFranquia);
+// router.get('/editar/:idJog', controllerFranquia.getEditarJog)
+// router.get('/deletar/:idJog', controllerFranquia.deletar)
+
+/* POST */
+// router.post('/editar/:idJog', upload.single('logo'), controllerFranquia.postEditarJog)
 
 module.exports = router;
